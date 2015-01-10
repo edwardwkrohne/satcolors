514694f5c2e22ecd644c64d08d0b259baaef2f4a
Ed Krohne <krohneew@gmail.com>
2015-01-10 00:58:09 -0600

    Licensed SATColors under GPL v3

--- src/graphs.cpp
+++ src/graphs.cpp
@@ -1,5 +1,28 @@
+// -*- Mode: C++ -*-
+////////////////////////////////////////////////////////////////////////////
+//
+//  satcolors -- for writably and readably building problems for SAT-solving
+//  Copyright (C) 2014 Edward W. Krohne III
+//
+//  The research that led to this product was partially supported by the
+//  U.S. NSF grants DMS-0943870 and DMS-1201290.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////
+//
 // April 2014
-
 // TODO: This file is a bit of a dumping ground for code that stacked
 // up in Solve.cpp recently.  I just don't need it for the moment.
 // It's just different kinds of graphs (in incidence matrix form) that
--- src/grid.h
+++ src/grid.h
@@ -1,4 +1,26 @@
-// -*- Mode:C++ -*-
+// -*- Mode: C++ -*-
+////////////////////////////////////////////////////////////////////////////
+//
+//  satcolors -- for writably and readably building problems for SAT-solving
+//  Copyright (C) 2014 Edward W. Krohne III
+//
+//  The research that led to this product was partially supported by the
+//  U.S. NSF grants DMS-0943870 and DMS-1201290.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////
 //
 // A 2d list of objects
 
